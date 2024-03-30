557f9350cc139607fa4f1a094cfdbd2ff7b41069
felipe Collyer <felipecollyer@hotmail.com>
2024-03-30 12:47:29 -0300

    add const p/ error

---
+++ src/usuario/errorMessages.ts
@@ -0,0 +1,12 @@
+const errorMessages = {
+  PASSWORDS_DO_NOT_MATCH: 'As senhas não coincidem.',
+  EMAIL_ALREADY_REGISTERED: 'Email já cadastrado.',
+  FAILED_TO_CREATE_USER: 'Erro ao criar usuário.',
+  FAILED_TO_UPDATE_USER: 'Erro ao atualizar usuário.',
+  FAILED_TO_LOGIN: 'Credenciais inválidas.',
+  FAILED_TO_USER: 'Usuario nao encontrado.',
+  ID_INVALID: 'ID invalido',
+  FAILED_IN_SEARCH: 'Erro ao buscar usuários:',
+};
+
+export { errorMessages };
